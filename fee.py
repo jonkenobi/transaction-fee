2621ee6de45226b16848ebf29c1bab639ac6b72f
jonkenobi <40736290+jonkenobi@users.noreply.github.com>
2020-08-08 13:10:52 +0900

    Update comments and notes in fee.py
    Makes notes that input validation would be possible; change variable name to be clearer.

--- fee.py
+++ fee.py
@@ -1,13 +1,13 @@
 #!python3
-# Returns the price raise needed to cover for exchange fees
+# Returns the price diff needed to cover for exchange fees given a certain purchase volume. 
 
 import sys
 
 EXCHANGE_FEE = 95 # price fee for GMO click s.
 
 # Usage----
-# fee.py Volume
-#where Volume is an int
+# fee.py stock_purchase_volume
+#Assumes stock_purchase_volume is an int; Obviously cleaner input validations should be required for a serious tool. 
 if len(sys.argv)>1:
-    Volume = int(sys.argv[1])
-    print(round(EXCHANGE_FEE/Volume,2))
+    stock_purchase_volume = int(sys.argv[1])
+    print(round(EXCHANGE_FEE/stock_purchase_volume,2))
